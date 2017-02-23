8a1616171d994a3151d8c9329be779c8be45bd0c
Igor Pavlov <gophan1992@gmail.com>
2017-02-23 13:49:03 +0300

    added tests

--- src/__tests__/TagContainer.test.js
+++ src/__tests__/TagContainer.test.js
@@ -12,7 +12,7 @@ describe('<TagContainer />', () => {
             "Санкт-Петербург", "Москва", "Новосибирск"
         ];
 
-        let wrapper = mount(<TagContainer tags={tags} />);
+        let wrapper = mount(<TagContainer tags={tags}/>);
         expect(wrapper.find(Tagger)).to.have.length(1);
     });
 
@@ -21,32 +21,47 @@ describe('<TagContainer />', () => {
             "Санкт-Петербург", "Москва", "Новосибирск"
         ];
 
-        let wrapper = mount(<TagContainer tags={tags} />);
+        let wrapper = mount(<TagContainer tags={tags}/>);
         expect(wrapper.find(TagElement)).to.have.length(3);
     });
 
     it('shouldn`t create TagElements if no tags sent', () => {
-        let wrapper = mount(<TagContainer tags={[]} />);
+        let wrapper = mount(<TagContainer tags={[]}/>);
         expect(wrapper.find(TagElement)).to.have.length(0);
     });
 
     it('should create TagElements from string', () => {
-        let wrapper = mount(<TagContainer tags="a,b,c" />);
+        let wrapper = mount(<TagContainer tags="a,b,c"/>);
         expect(wrapper.find(TagElement)).to.have.length(3);
     });
 
     it('should use delimeters string', () => {
-        let wrapper = mount(<TagContainer tags="a:b:c" delimeters=":" />);
+        let wrapper = mount(<TagContainer tags="a:b:c" delimeters=":"/>);
         expect(wrapper.find(TagElement)).to.have.length(3);
     });
 
     it('should use delimeters array', () => {
-        let wrapper = mount(<TagContainer tags="a:b:c" delimeters={[':']} />);
+        let wrapper = mount(<TagContainer tags="a:b:c" delimeters={[':']}/>);
         expect(wrapper.find(TagElement)).to.have.length(3);
     });
 
     it('should use custom delimeters if set', () => {
-        let wrapper = mount(<TagContainer tags="a:b:c,d,e,f" delimeters={[':']} />);
+        let wrapper = mount(<TagContainer tags="a:b:c,d,e,f" delimeters={[':']}/>);
         expect(wrapper.find(TagElement)).to.have.length(3);
-    })
+    });
+
+    it('should filter duplicate tags', () => {
+        let wrapper = mount(<TagContainer tags="a,b,c,c,b,b,b,a,a"/>);
+        expect(wrapper.find(TagElement)).to.have.length(3);
+    });
+
+    it('should use special delimeters', () => {
+        let wrapper = mount(<TagContainer tags="a$b$c$d" delimeters="$"/>);
+        expect(wrapper.find(TagElement)).to.have.length(4);
+    });
+
+    it('should use many delimeters', () => {
+        let wrapper = mount(<TagContainer tags="a$b$c,d:e" delimeters="$,:"/>);
+        expect(wrapper.find(TagElement)).to.have.length(5);
+    });
 });
