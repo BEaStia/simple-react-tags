549c04b6a68a9e3b4f131ce2dbfd3e904656d769
Igor Pavlov <gophan1992@gmail.com>
2017-02-23 11:41:26 +0300

    added more tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from '../components/App/App';
 import Tagger from '../components/Tagger/Tagger'
+import TagElement from '../components/TagElement/TagElement'
 import TagContainer from '../components/TagContainer/TagContainer'
 import { mount, shallow } from 'enzyme';
 import {expect} from 'chai';
 
 describe('<App />', () => {
-    it('works', () => {
+    it('should create tagContainer', () => {
+        let wrapper = mount(<App />);
+        expect(wrapper.find(TagContainer)).to.have.length(1);
+    });
+
+    it('should pass tags to tagContainer', ()=> {
         const tags = [
             "Санкт-Петербург", "Москва", "Новосибирск"
         ];
 
         let wrapper = mount(<App tags={tags} />);
         expect(wrapper.find(TagContainer)).to.have.length(1);
-        console.log(wrapper.state());
-        console.log(wrapper.find(<TagContainer />));
+        let container = wrapper.find(TagContainer);
+        expect(container.props().tags).to.equal(tags);
     });
 });
---
+++ src/__tests__/TagContainer.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import App from '../components/App/App';
+import Tagger from '../components/Tagger/Tagger'
+import TagElement from '../components/TagElement/TagElement'
+import TagContainer from '../components/TagContainer/TagContainer'
+import { mount, shallow } from 'enzyme';
+import {expect} from 'chai';
+
+describe('<TagContainer />', () => {
+    it('should create Tagger', () => {
+        const tags = [
+            "Санкт-Петербург", "Москва", "Новосибирск"
+        ];
+
+        let wrapper = mount(<TagContainer tags={tags} />);
+        expect(wrapper.find(Tagger)).to.have.length(1);
+    });
+
+    it('should create TagElements', () => {
+        const tags = [
+            "Санкт-Петербург", "Москва", "Новосибирск"
+        ];
+
+        let wrapper = mount(<TagContainer tags={tags} />);
+        expect(wrapper.find(TagElement)).to.have.length(3);
+    });
+
+    it('shouldn`t create TagElements if no tags sent', () => {
+        let wrapper = mount(<TagContainer tags={[]} />);
+        expect(wrapper.find(TagElement)).to.have.length(0);
+    });
+
+    it('should create TagElements from string', () => {
+        let wrapper = mount(<TagContainer tags="a,b,c" />);
+        expect(wrapper.find(TagElement)).to.have.length(3);
+    });
+
+    it('should use delimeters string', () => {
+        let wrapper = mount(<TagContainer tags="a:b:c" delimeters=":" />);
+        expect(wrapper.find(TagElement)).to.have.length(3);
+    });
+
+    it('should use delimeters array', () => {
+        let wrapper = mount(<TagContainer tags="a:b:c" delimeters={[':']} />);
+        expect(wrapper.find(TagElement)).to.have.length(3);
+    });
+
+    it('should use custom delimeters if set', () => {
+        let wrapper = mount(<TagContainer tags="a:b:c,d,e,f" delimeters={[':']} />);
+        expect(wrapper.find(TagElement)).to.have.length(3);
+    })
+});
